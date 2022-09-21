be738043b006c881532b991513c35eaa8ebbfa9e
Gabriel Wilkins <wilkigab001@gmail.com>
2022-09-21 16:32:18 -0600

    Yep

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ require('dotenv').config()
 
 const path = require('path')
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
+app.use(express.static('client'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
