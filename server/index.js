26e651fad3bd194e5543a2b3e742593839de05c1
Gabriel Wilkins <wilkigab001@gmail.com>
2022-09-21 16:47:01 -0600

    Final test :

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
-app.use(express.static('client'))
+//app.use(express.static('client'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
