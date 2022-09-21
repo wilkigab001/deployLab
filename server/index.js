717659018483ce1c0aef51f8e4b119d9cfb54e9f
Gabriel Wilkins <wilkigab001@gmail.com>
2022-09-21 16:35:14 -0600

    Added css to index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
+})
+
 
 
 app.listen(port, () => {
